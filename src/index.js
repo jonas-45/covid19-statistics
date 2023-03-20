ba9b7333312dbafc036365f77e6a9d7039bc152f
jonas-45 <jonaswullah@gmail.com>
2023-03-20 15:16:43 +0000

    Install and import react bootstrap

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import BrowserRouter from 'react-router-dom'
 import { store } from './app/store';
 import App from './App';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/react-bootstrap/dist/react-bootstrap';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 

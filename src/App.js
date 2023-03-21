71dd2d389d12670cd04d718b3b501f9400991119
jonas-45 <jonaswullah@gmail.com>
2023-03-21 17:05:48 +0000

    Call useDispatch and useEffect hooks in app.js to retrieve api data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router';
+import { useDispatch } from 'react-redux';
 import Root from './components/Root';
 import './App.css';
 import Leagues from './pages/Leagues';
 import LeagueDetail from './pages/LeagueDetail';
 import NoMatch from './pages/NoMatch';
+import { getLeagues } from './redux/leagues/leaguesSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getLeagues());
+  }, [dispatch]);
   return (
     <Routes>
       <Route path="/" element={<Root />}>

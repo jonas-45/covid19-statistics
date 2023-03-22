253e19808b61d8d91176b58e4445a2ab7e8e9879
jonas-45 <jonaswullah@gmail.com>
2023-03-22 04:06:34 +0000

    Add form and Nav from react-bootstrap

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Container, Navbar, Offcanvas } from 'react-bootstrap';
+import {
+  Container, Navbar, Offcanvas, Nav, Form,
+} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import logo from '../images/sports-leagues.png';
@@ -14,7 +16,7 @@ const NavBar = () => {
       <Container fluid>
         <Navbar.Brand>
           <img src={logo} alt="logo" className="d-inline-block" style={{ width: '80px' }} />
-          <span>Sports Leagues</span>
+          <span>Covid19 Statistics</span>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={toggleCanvas} />
         <Navbar.Offcanvas show={showCanvas} onHide={handleClose}>
@@ -22,9 +24,12 @@ const NavBar = () => {
             <Offcanvas.Title>Menu</Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
-            <nav className="d-flex justify-content-end flex-grow-1 pe-5">
-              <NavLink className="navlink" to="/"><FontAwesomeIcon icon={faUserCircle} size="3x" /></NavLink>
-            </nav>
+            <Nav className="d-flex justify-content-between align-items-center flex-grow-1 pe-5">
+              <Form inline>
+                <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
+              </Form>
+              <NavLink className="navlink" to="/"><FontAwesomeIcon icon={faUserCircle} size="2x" /></NavLink>
+            </Nav>
           </Offcanvas.Body>
         </Navbar.Offcanvas>
       </Container>

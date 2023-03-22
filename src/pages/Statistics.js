079278af6c3289c96d8715c28b2947e815a6e269
jonas-45 <jonaswullah@gmail.com>
2023-03-22 12:30:43 +0000

    Display filtered data in statistics component

--- src/pages/Statistics.js
+++ src/pages/Statistics.js
@@ -5,17 +5,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const Statistics = () => {
-  const { covidStats } = useSelector((state) => state.covid);
+  const { covidStats, filtered } = useSelector((state) => state.covid);
+
   if (covidStats.length < 1) {
     return (
-      <div>
+      <div className="text-center mt-5 pt-5">
         <p>Loading data, please wait...</p>
       </div>
     );
   }
   return (
     <div className="d-flex flex-wrap justify-content-center gap-3 p-3">
-      {covidStats.map((info) => (
+      {console.log('stats page filtered: ', filtered)}
+      {filtered.map((info) => (
         <Card style={{ width: '18rem' }} key={info.id} className="bg-light p-2">
           <Card.Img variant="top" src={info.flag} style={{ width: '100%', height: '50%' }} />
           <Card.Body>

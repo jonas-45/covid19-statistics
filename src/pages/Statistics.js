8d3d4bb57f84f74755344c4468a7cae93a4f3e4d
jonas-45 <jonaswullah@gmail.com>
2023-03-22 04:10:16 +0000

    Remove flag placeholder

--- src/pages/Statistics.js
+++ src/pages/Statistics.js
@@ -3,7 +3,6 @@ import { Card } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleArrowRight } from '@fortawesome/free-solid-svg-icons';
-import placeholder from '../images/placeholder.png';
 
 const Statistics = () => {
   const { covidStats } = useSelector((state) => state.covid);
@@ -18,7 +17,7 @@ const Statistics = () => {
     <div className="d-flex flex-wrap justify-content-center gap-3 p-3">
       {covidStats.map((info) => (
         <Card style={{ width: '18rem' }} key={info.id} className="bg-light p-2">
-          <Card.Img variant="top" src={info.flag ? info.flag : placeholder} style={{ width: '100%', height: '50%' }} />
+          <Card.Img variant="top" src={info.flag} style={{ width: '100%', height: '50%' }} />
           <Card.Body>
             <Card.Title>{info.country }</Card.Title>
             <div className="d-flex justify-content-between align-items-center">

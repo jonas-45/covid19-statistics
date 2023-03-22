1064a5a1e00152a6213cd369b5b0f6006c64ec65
jonas-45 <jonaswullah@gmail.com>
2023-03-22 04:08:13 +0000

    Fetch data from api

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import { Routes, Route } from 'react-router';
 import { useDispatch } from 'react-redux';
 import Root from './components/Root';
 import './App.css';
-import Leagues from './pages/Leagues';
 import LeagueDetail from './pages/LeagueDetail';
 import NoMatch from './pages/NoMatch';
-import { getLeagues } from './redux/leagues/leaguesSlice';
+import { getCovideStatistics } from './redux/covid/covidSlice';
+import Statistics from './pages/Statistics';
 
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getLeagues());
+    dispatch(getCovideStatistics());
   }, [dispatch]);
   return (
     <Routes>
       <Route path="/" element={<Root />}>
-        <Route index element={<Leagues />} />
-        <Route path="/leaguedetails" element={<LeagueDetail />} />
+        <Route index element={<Statistics />} />
+        <Route path="/detail" element={<LeagueDetail />} />
         <Route path="*" element={<NoMatch />} />
       </Route>
     </Routes>

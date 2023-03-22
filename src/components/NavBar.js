0b2a336fb1c6f1ea51996829ed0d28d921ff08d0
jonas-45 <jonaswullah@gmail.com>
2023-03-22 12:33:17 +0000

    Add styles to navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import {
   Container, Navbar, Offcanvas, Nav, Form,
 } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { useDispatch } from 'react-redux';
 import logo from '../images/sports-leagues.png';
+import { searchByCountryName } from '../redux/covid/covidSlice';
 
 const NavBar = () => {
+  const dispatch = useDispatch();
   const [showCanvas, setShowCanvas] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
   const handleClose = () => setShowCanvas(false);
   const toggleCanvas = () => setShowCanvas((s) => !s);
+  const updateSearchTerm = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  useEffect(() => {
+    dispatch(searchByCountryName(searchTerm));
+  }, [searchTerm, dispatch]);
   return (
-    <Navbar collapseOnSelect expand="lg" bg="light">
+    <Navbar collapseOnSelect expand="lg" bg="light" fixed="top" className="mb-5">
       <Container fluid>
         <Navbar.Brand>
-          <img src={logo} alt="logo" className="d-inline-block" style={{ width: '80px' }} />
+          <img src={logo} alt="logo" className="d-inline-block" style={{ width: '60px' }} />
           <span>Covid19 Statistics</span>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={toggleCanvas} />
@@ -24,9 +34,9 @@ const NavBar = () => {
             <Offcanvas.Title>Menu</Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
-            <Nav className="d-flex justify-content-between align-items-center flex-grow-1 pe-5">
+            <Nav className="d-flex justify-content-between align-items-center flex-grow-1 pe-4">
               <Form inline>
-                <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
+                <Form.Control type="text" placeholder="Search" onChange={updateSearchTerm} className="mr-sm-2" />
               </Form>
               <NavLink className="navlink" to="/"><FontAwesomeIcon icon={faUserCircle} size="2x" /></NavLink>
             </Nav>

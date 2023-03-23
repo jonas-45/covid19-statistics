f633cdfa20f5972208333c673e74ca1fd407dddc
jonas-45 <jonaswullah@gmail.com>
2023-03-23 15:08:35 +0000

    Resolve linter errors

--- src/pages/CovidStatDetails.js
+++ src/pages/CovidStatDetails.js
@@ -16,76 +16,73 @@ const CovidStatDetails = () => {
         <div className="text-left">
           <FontAwesomeIcon icon={faArrowAltCircleLeft} size="2x" className="text-primary mb-2" onClick={() => navigate(-1)} style={{ cursor: 'pointer' }} />
         </div>
-        <div>
-          {(id === undefined || id === null)}
-        </div>
         <Card className="text-center bg-light p-3" style={{ width: '40rem' }}>
-          <Card.Img src={detailArr[0].flag} variant="top" style={{ width: '100%' }} />
+          <Card.Img src={detailArr.flag} variant="top" style={{ width: '100%' }} />
           <Card.Body>
-            <Card.Title><h1 className="text-primary">{detailArr[0].country}</h1></Card.Title>
+            <Card.Title><h1 className="text-primary">{detailArr.country}</h1></Card.Title>
             <div className="d-flex flex-wrap gap-5 mt-5">
               <Badge bg="success" className="p-2 fs-6 text-center">
                 <span className="text-light">
                   Continent:
                   {' '}
                 </span>
-                <span className="text-info">{detailArr[0].continent}</span>
+                <span className="text-info">{detailArr.continent}</span>
               </Badge>
               <Badge bg="success" className="p-2 fs-6 text-center">
                 <span className="text-light">
                   Polulation:
                   {' '}
                 </span>
-                <span className="text-info">{detailArr[0].population}</span>
+                <span className="text-info">{detailArr.population}</span>
               </Badge>
               <Badge bg="success" className="p-2 fs-6 text-center">
                 <span className="text-light">
                   Critical:
                   {' '}
                 </span>
-                <span className="text-info">{detailArr[0].critical}</span>
+                <span className="text-info">{detailArr.critical}</span>
               </Badge>
               <Badge bg="success" className="p-2 fs-6 text-center">
                 <span className="text-light">
                   Total Cases:
                   {' '}
                 </span>
-                <span className="text-info">{detailArr[0].cases}</span>
+                <span className="text-info">{detailArr.cases}</span>
               </Badge>
               <Badge bg="success" className="p-2 fs-6 text-center">
                 <span className="text-light">
                   Total Deaths:
                   {' '}
                 </span>
-                <span className="text-info">{detailArr[0].deaths}</span>
+                <span className="text-info">{detailArr.deaths}</span>
               </Badge>
               <Badge bg="success" className="p-2 fs-6 text-center">
                 <span className="text-light">
                   Cases Today:
                   {' '}
                 </span>
-                <span className="text-info">{detailArr[0].today}</span>
+                <span className="text-info">{detailArr.today}</span>
               </Badge>
               <Badge bg="success" className="p-2 fs-6 text-center">
                 <span className="text-light">
                   Deaths Today:
                   {' '}
                 </span>
-                <span className="text-info">{detailArr[0].deathstoday}</span>
+                <span className="text-info">{detailArr.deathstoday}</span>
               </Badge>
               <Badge bg="success" className="p-2 fs-6 text-center">
                 <span className="text-light">
                   Active:
                   {' '}
                 </span>
-                <span className="text-info">{detailArr[0].active}</span>
+                <span className="text-info">{detailArr.active}</span>
               </Badge>
               <Badge bg="success" className="p-2 fs-6 text-center">
                 <span className="text-light">
                   Total Tests:
                   {' '}
                 </span>
-                <span className="text-info">{detailArr[0].tests}</span>
+                <span className="text-info">{detailArr.tests}</span>
               </Badge>
             </div>
           </Card.Body>

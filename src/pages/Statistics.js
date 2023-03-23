d2dcf763b4b3b17edc318cb94a86e98b5389b6b7
jonas-45 <jonaswullah@gmail.com>
2023-03-23 06:22:38 +0000

    Add navlink to more icon for each data item

--- src/pages/Statistics.js
+++ src/pages/Statistics.js
@@ -3,6 +3,8 @@ import { Card } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { NavLink } from 'react-router-dom';
+import NavBar from '../components/NavBar';
 
 const Statistics = () => {
   const { covidStats, filtered } = useSelector((state) => state.covid);
@@ -15,29 +17,31 @@ const Statistics = () => {
     );
   }
   return (
-    <div className="d-flex flex-wrap justify-content-center gap-3 p-3">
-      {console.log('stats page filtered: ', filtered)}
-      {filtered.map((info) => (
-        <Card style={{ width: '18rem' }} key={info.id} className="bg-light p-2">
-          <Card.Img variant="top" src={info.flag} style={{ width: '100%', height: '50%' }} />
-          <Card.Body>
-            <Card.Title>{info.country }</Card.Title>
-            <div className="d-flex justify-content-between align-items-center">
-              <Card.Subtitle>
-                Deaths:
+    <div className="d-flex flex-column gap-3">
+      <NavBar />
+      <div className="d-flex flex-wrap justify-content-center gap-3 p-3">
+        {filtered.map((info) => (
+          <Card style={{ width: '18rem' }} key={info.id} className="bg-light p-2">
+            <Card.Img variant="top" src={info.flag} style={{ width: '100%', height: '50%' }} />
+            <Card.Body>
+              <Card.Title className="text-info">{info.country }</Card.Title>
+              <div className="d-flex justify-content-between align-items-center">
+                <Card.Subtitle>
+                  Deaths:
+                  {' '}
+                  {info.deaths}
+                </Card.Subtitle>
+                <Card.Subtitle><NavLink to={`/${info.country}/detail/${info.id}`}><FontAwesomeIcon icon={faCircleArrowRight} size="2x" /></NavLink></Card.Subtitle>
+              </div>
+              <Card.Text className="mt-3">
+                Polulation:
                 {' '}
-                {info.deaths}
-              </Card.Subtitle>
-              <Card.Subtitle><FontAwesomeIcon icon={faCircleArrowRight} size="2x" /></Card.Subtitle>
-            </div>
-            <Card.Text className="mt-3">
-              Polulation:
-              {' '}
-              {info.population}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      ))}
+                {info.population}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };

b0a0f73fd1ac4c5b6528ac44d2bda00a9dde6b18
jonas-45 <jonaswullah@gmail.com>
2023-03-23 06:23:20 +0000

    Add styles to noMatch component

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const NoMatch = () => (
-  <div>Page not available</div>
+  <div className="text-center mt-5 pt-5">
+    <h1>Page not available</h1>
+  </div>
 );
 
 export default NoMatch;
